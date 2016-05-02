d2ee3c1d516eb0092e7b01636e7f8051a651080a
Fabien Quatravaux <fabien.quatravaux@riseup.net>
2016-05-02 17:19:41 +0200

    [init] Première appli fonctionnelle

---
+++ js/auto.js
@@ -0,0 +1,52 @@
+(function(window, document){
+	var nbImg = 4, slide = '', time=0;
+	var nodes = {
+		root : document.querySelector('#impress'),
+		body : document.querySelector('body'),
+		timer : document.querySelector('#img-timer')
+	};
+	var size = {
+		w : window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
+		h : window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
+	};
+	var api = impress()
+
+	// Slides creation
+	for (var i=0; i<nbImg; i++) {
+		slide = '<div class="step slide" data-x="' + (size.w*2+200)*i + '" data-y="-'+ (size.h+200) +'">';
+		slide += '<img src="img/' + (i+1) + '.jpg"/>';
+		slide += '</div>';
+		nodes.temp = document.createElement('div');
+		nodes.temp.innerHTML = slide;
+		nodes.root.appendChild(nodes.temp.childNodes[0]);
+		delete nodes.temp;
+	}
+
+	// Launch impress
+	api.init();
+	
+	// TODO : gérer la pause
+	
+	setInterval(function(){
+		time++;
+		nodes.timer.childNodes[0].innerHTML = time;
+
+		if(time / 5 < 1){
+			nodes.timer.style.borderColor = 'transparent';
+		}
+		if(time / 5 == 1){
+			nodes.timer.style.borderTopColor = 'rgba(0,183,229,0.9)';
+		}
+		if(time / 5 == 2){
+			nodes.timer.style.borderRightColor = 'rgba(0,183,229,0.9)';
+		}
+		if(time / 5 == 3){
+			nodes.timer.style.borderBottomColor = 'rgba(0,183,229,0.9)';
+		}
+		if(time / 5 == 4){
+			nodes.timer.style.borderLeftColor = 'rgba(0,183,229,0.9)';
+			api.next();
+			time = 0;
+		}
+	}, 1000);
+})(window, document);
