77a95560997f34e83e857f5655425e0ab1bd1c1d
Fabien Quatravaux <fabien.quatravaux@riseup.net>
2016-05-03 13:25:02 +0200

    [dev] Gérer la fonction play/pause

--- js/auto.js
+++ js/auto.js
@@ -1,5 +1,5 @@
 (function(window, document){
-	var nbImg = 4, slide = '', time=0;
+	var nbImg = 4, slide = '', time=0, timer;
 	var nodes = {
 		root : document.querySelector('#impress'),
 		body : document.querySelector('body'),
@@ -22,31 +22,47 @@
 		delete nodes.temp;
 	}
 
+	// wire up the play button
+	nodes.timer.addEventListener('click', function(){
+		if( nodes.timer.className == "") {
+			nodes.timer.className = "play-btn";
+			stopTimer();
+		} else {
+			nodes.timer.className = "";
+			launchTimer();
+		}
+	});
+
 	// Launch impress
 	api.init();
-	
-	// TODO : gérer la pause
-	
-	setInterval(function(){
-		time++;
-		nodes.timer.childNodes[0].innerHTML = time;
 
-		if(time / 5 < 1){
-			nodes.timer.style.borderColor = 'transparent';
-		}
-		if(time / 5 == 1){
-			nodes.timer.style.borderTopColor = 'rgba(0,183,229,0.9)';
-		}
-		if(time / 5 == 2){
-			nodes.timer.style.borderRightColor = 'rgba(0,183,229,0.9)';
-		}
-		if(time / 5 == 3){
-			nodes.timer.style.borderBottomColor = 'rgba(0,183,229,0.9)';
-		}
-		if(time / 5 == 4){
-			nodes.timer.style.borderLeftColor = 'rgba(0,183,229,0.9)';
-			api.next();
-			time = 0;
-		}
-	}, 1000);
+	function launchTimer(){
+		time = parseInt(nodes.timer.childNodes[0].innerHTML, 10);
+		timer = setInterval(function(){
+			time++;
+			nodes.timer.childNodes[0].innerHTML = time;
+
+			if(time / 5 < 1){
+				nodes.timer.style.borderColor = 'transparent';
+			}
+			if(time / 5 == 1){
+				nodes.timer.style.borderTopColor = 'rgba(0,183,229,0.9)';
+			}
+			if(time / 5 == 2){
+				nodes.timer.style.borderRightColor = 'rgba(0,183,229,0.9)';
+			}
+			if(time / 5 == 3){
+				nodes.timer.style.borderBottomColor = 'rgba(0,183,229,0.9)';
+			}
+			if(time / 5 == 4){
+				nodes.timer.style.borderLeftColor = 'rgba(0,183,229,0.9)';
+				api.next();
+				time = 0;
+			}
+		}, 1000);
+	}
+
+	function stopTimer(){
+		timer = clearInterval(timer);
+	}
 })(window, document);
